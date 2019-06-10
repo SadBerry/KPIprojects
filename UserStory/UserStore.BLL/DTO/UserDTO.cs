cca9a15dcc95a90efedc25dbc0aac90dcbde0b61
SadBerry <darinasaliy157@gmail.com>
2019-06-10 16:44:37 +0300

    d

--- UserStory/UserStore.BLL/DTO/UserDTO.cs
+++ UserStory/UserStore.BLL/DTO/UserDTO.cs
@@ -9,6 +9,6 @@
         public string Name { get; set; }
         public string Address { get; set; }
         public string Role { get; set; }
-        public string Role1 { get; set; }
+
     }
 }
